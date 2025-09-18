9fac1862cc87f1708ba83efaa057e456737f4e69
luthfikhaerii <luthfikhaeri00@gmail.com>
2025-09-18 19:49:36 +0700

    create exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.setGlobalPrefix('api')
+  app.useGlobalFilters(new HttpExceptionFilter)
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
