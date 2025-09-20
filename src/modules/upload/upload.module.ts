9d0762a7a0b7fa0d2515427738a1138b6e578091
luthfikhaerii <luthfikhaeri00@gmail.com>
2025-09-20 19:26:18 +0700

    fix upload service

--- src/modules/upload/upload.module.ts
+++ src/modules/upload/upload.module.ts
@@ -3,8 +3,8 @@ import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
 
 @Module({
-    imports: [UploadService],
-    providers:[UploadService],
+    // imports: [UploadService],
+    // providers:[UploadService],
     controllers: [UploadController]
 })
 export class UploadModule {}
